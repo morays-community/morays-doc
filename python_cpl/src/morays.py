3755ed8b84023e0508ba73da3c64013095f2d77a
Alexis Barge <alexis.barge@proton.me>
2023-10-13 16:16:01 +0200

    first design of API

---
+++ python_cpl/src/morays.py
@@ -0,0 +1,52 @@
+#import argparse
+from smart_morays.pycpl import open_tunnel, Tunnel
+from smart_morays.inferences import Model
+from smart_morays.loop import init_morays, coupled_run, finish_morays, Freqs
+
+def main():
+
+    #  Init Morays
+    # +++++++++++++
+    init_morays()
+
+    #  Couple Ocean
+    # ++++++++++++++
+    tunnel_config = dict({ 'label' : 'Morays', \
+                           'grids' : dict({'torc' : [720,603] ,\
+                                           'lmdz' : [180,151] }),
+                           'vars' : [  dict({'in' : 'sst', 'out' : 'sst_var', 'grd' : 'torc', 'lvl' : 1, 'freq' : Freqs.DAILY }),  \
+                                       dict({'in' : 'svt', 'out' : 'svt_var', 'grd' : 'torc', 'lvl' : 3, 'freq' : Freqs.DAILY }) ] \
+                        })
+                
+    nemo = open_tunnel(**tunnel_config)
+
+    #  Couple Model
+    # ++++++++++++++
+    model_config = dict({ 'label'    : '', \
+                          'weights'  : '', \
+                          'features' : [], \
+                          'preds'    : [], \
+                        })
+    
+    
+    gtf = LTPytorch_Model('GeoTrainFlow')
+
+    #  Run Simulation
+    # ++++++++++++++++
+    
+    # namelist
+    namelist = nml.read('namelist_cfg')
+
+    time_step = int( namelist['namdom']['rn_Dt']  )
+    niter = int( namelist['namrun']['nn_itend'] - namelist['namrun']['nn_it000'] )
+
+    coupled_run(nemo,gtf,time_step,niter)
+    #uncoupled_run(gtf)
+
+    #  Terminate
+    # +++++++++++
+    finish_morays(nemo,gtf)
+ 
+ 
+if __name__=='__main__':
+   main()
---
+++ python_cpl/src/smart_morays/inferences.py
@@ -0,0 +1,56 @@
+import logging
+import pytorch_lightning as lt
+
+logger = logging.getLogger(__name__)
+
+
+class TrainedModel:
+
+    def __init__(self):
+        pass
+    
+    def __del__(self):
+        pass
+
+    def load_data
+
+    def load_weight(self):
+        pass
+
+
+class Pytorch_Model(TrainedModel):
+
+    def __init__(self):
+        pass
+        
+    def __del__(self):
+        pass
+        
+        
+class LTPytorch_Model(TrainedModel):
+
+    def __init__(self, label, DataManager, LightningModule):
+        self.DM = DataManager
+        self.LTM = LTModule
+        
+    def __del__(self):
+        pass
+
+
+class TensorFlow_Model(TrainedModel):
+
+    def __init__(self):
+        logging.error('  Tensor Flow models not supported yet')
+        quit()
+        
+    def __del__(self):
+        pass
+
+
+class UDF_Model(TrainedModel):
+
+    def __init__():
+        pass
+        
+    def __del__():
+        pass
