5c098446386bd0c57a9f962cd4f8982aff00fdbd
Alexis Barge <138531178+alexis-barge@users.noreply.github.com>
2024-10-01 09:53:33 +0200

    remove release number

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2024, Alexis Barge, Julien Le Sommer'
 author = 'Alexis Barge, Julien Le Sommer'
 
 # The full version, including alpha/beta/rc tags
-release = '0.9.0'
+# release = '0.9.0'
 
 
 # -- General configuration ---------------------------------------------------
